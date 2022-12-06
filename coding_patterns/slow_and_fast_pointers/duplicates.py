cd22f8aeab9c86a8cc6c9db5cc6640381e4197eb
tofetpuzo <topetemiola09@gmail.com>
2022-12-06 12:32:58 +0000

    solution

--- coding_patterns/slow_and_fast_pointers/duplicates.py
+++ coding_patterns/slow_and_fast_pointers/duplicates.py
@@ -1,6 +1,29 @@
-def duplicates():
+from typing import List
+
+
+def duplicates(nums: List[int]):
     # this is a linked list algorithm
     #
 
     # find the point the two intersect at 
-    pass
+    slow , fast = 0, 0
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+        else:
+            return False
+
+
+    slow2 = 0
+    while True:
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if slow == slow2:
+            break
+        else:
+            return False
+    
+    return True
+
