032595cd88b381f711e0eb4805b1116a67796639
tofetpuzo <topetemiola09@gmail.com>
2022-12-06 13:12:05 +0000

    solution

--- coding_patterns/slow_and_fast_pointers/duplicates.py
+++ coding_patterns/slow_and_fast_pointers/duplicates.py
@@ -8,22 +8,35 @@ def duplicates(nums: List[int]):
     # find the point the two intersect at 
     slow , fast = 0, 0
     while True:
+        if (slow < 0 or fast < 0 or slow >len(nums) or fast >len(nums)):
+            return False
         slow = nums[slow]
         fast = nums[nums[fast]]
         if slow == fast:
-            break
-        else:
-            return False
+            return True
 
+        fast = nums[nums[fast]]
 
-    slow2 = 0
-    while True:
-        slow = nums[slow]
-        slow2 = nums[slow2]
-        if slow == slow2:
-            break
-        else:
-            return False
+        if slow == fast:
+            return True
+   
+        return False
     
-    return True
 
+    # while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        # if slow == fast:
+        #     break
+    # slow2 = 0
+    # while True:
+    #     slow = nums[slow]
+    #     slow2 = nums[slow2]
+    #     if slow == slow2:
+    #         return True
+    #     else:
+    #         return False
+    
+nums = [2, -1, 1, 2]
+
+print(duplicates(nums))
