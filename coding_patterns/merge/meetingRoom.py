433a5414923d615c8841d945f320eff4f5c5caa0
tofetpuzo <topetemiola09@gmail.com>
2022-12-07 11:36:31 +0000

    solution

---
+++ coding_patterns/merge/meetingRoom.py
@@ -0,0 +1,11 @@
+# given an array of meeting time intervals consisting of start and end times [[s1, e1], [s2, e2]]
+
+# find mininum number of conference room required
+# 
+# interval = [(0, 30), (5, 10), (15, 20)]
+# output = 2
+# explanation we need two meeting rooms 
+# room1 : (0, 30)
+# room2: (5, 10), (15, 20) 
+def meetingRoom():
+    pass
