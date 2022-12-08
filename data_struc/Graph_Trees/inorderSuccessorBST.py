6bb9a5db5c882b8e56372981fa41a47bb31fd184
tofetpuzo <topetemiola09@gmail.com>
2022-12-08 12:02:08 +0000

    solution

---
+++ data_struc/Graph_Trees/inorderSuccessorBST.py
@@ -0,0 +1,8 @@
+# given the root of a binary search tree and a node p in it, return the inorder successor of that node
+# in the binary search tree if the given node has no in-order successor in the tree, return null
+# the successor of a node p in the node with the smallest key greater than p.val
+
+
+# inoder travesal: always in sorted order
+def inorderSuccessorBST():
+    pass
