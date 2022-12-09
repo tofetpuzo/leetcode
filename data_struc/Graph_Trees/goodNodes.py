70fe7c04bfbcd6e2e6a2dfd68e80d0ed13781899
tofetpuzo <topetemiola09@gmail.com>
2022-12-09 04:34:45 +0000

    solution

--- data_struc/Graph_Trees/goodNodes.py
+++ data_struc/Graph_Trees/goodNodes.py
@@ -1,4 +1,32 @@
 # Given a binary tree root, a node X in the tree is named good, if in the path from root to X
 # there are no nodes with a value greater than X, return the number of good nodes in the binary tree
-def goodNodes():
-    pass
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+def goodNodes(root: TreeNode, ):
+    if not root:
+        return 0
+    good_nodes = 0
+    
+    
+    
+    def dfs(node, max_seen):
+        if not node:
+            return
+        
+        if root.value >= max_seen:
+            good_nodes +=1
+            
+            max_seen = max(max_seen, node.value)
+        
+        dfs(root.left, max_seen)
+        dfs(root.right, max_seen)
+            
+        
+        
+    dfs(root, 10001)
+    return good_nodes    
+        
---
+++ data_struc/Graph_Trees/maxSumTreePath.py
@@ -0,0 +1,2 @@
+def maxSumTreePath():
+    pass
