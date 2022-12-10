969e52c21524aa6f01e9fd8cdc703f522d6c2145
tofetpuzo <topetemiola09@gmail.com>
2022-12-10 00:21:19 +0000

    solution

---
+++ data_struc/Graph_Trees/LCABSTIII.py
@@ -0,0 +1,29 @@
+# given two node of a binary tree p and q return their lowest common ancestor , each node
+# will have a reference to its parent node. the def for node is below
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+        self.parent = None
+
+# this T: O(N)
+# S: O(N)
+def lowestCommonAncestor(p: Node, q: Node):
+    seen = set()
+    while p:
+        seen.add(p)
+        p = p.parent
+
+    while q:
+        if q in seen:
+            return q
+
+        q = q.parent
+
+# T: O(1)
+
+def lowestCommonAncestor2(p: Node, q: Node):
+    p_copy = p
+    q_copy = q
+    
