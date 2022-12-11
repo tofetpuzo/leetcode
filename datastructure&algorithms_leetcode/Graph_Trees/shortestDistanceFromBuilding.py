a13f4d1027caa445a6bee34bdc45cc000a5b42ee
tofetpuzo <topetemiola09@gmail.com>
2022-12-11 14:31:29 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/shortestDistanceFromBuilding.py
+++ datastructure&algorithms_leetcode/Graph_Trees/shortestDistanceFromBuilding.py
@@ -19,3 +19,50 @@
 # Explanation: Given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0,2),
 # the point (1,2) is an ideal empty land to build a house, as the total
 # travel distance of 3+3+1=7 is minimal. So return 7.
+import collections
+from typing import List
+
+
+def shortestDistanceFromBuilding(grid: List[List[int]]):
+    ROWS , COLS = len(grid), len(grid[0])
+
+    dist_matrix = [[0] * COLS for row in range(ROWS)]
+    if not grid:
+        return 0
+    
+    directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+
+    BUILDING = 1
+    OBSTACLE = 2
+    EMPTY_LAND = 0
+    min_dist = float("inf")
+
+
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == BUILDING:
+                local_dist = float("inf")
+                q = collections.deque([(row, col , 0)])
+
+
+                while q:
+                    cur_row, cur_col, dist = q.popleft()
+                    for dr, dc in directions:
+                        r, c = dr+cur_row , dc+cur_col
+                        if (r in range(ROWS) and c in range(COLS) and grid[r][c] == EMPTY_LAND):
+                            grid[r][c] -=1
+                            dist_matrix[r][c] += dist + 1
+                            q.append((r, c, dist + 1))
+                            
+                           
+                            local_dist = min(local_dist, dist_matrix[r][c])
+                            
+
+                EMPTY_LAND-=1
+                min_dist = local_dist
+
+    return min_dist if min_dist != float("inf") else -1
+
+grid = [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
+
+print(shortestDistanceFromBuilding(grid))
