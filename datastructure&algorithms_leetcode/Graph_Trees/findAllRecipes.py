9c24ef190e66106ac74b0ab11e30bc4c7fa47f52
tofetpuzo <topetemiola09@gmail.com>
2022-12-12 17:54:05 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/findAllRecipes.py
+++ datastructure&algorithms_leetcode/Graph_Trees/findAllRecipes.py
@@ -1,4 +1,6 @@
-# You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. Ingredients to a recipe may need to be created from other recipes, i.e., ingredients[i] may contain a string that is in recipes.
+# You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. 
+# The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. 
+# Ingredients to a recipe may need to be created from other recipes, i.e., ingredients[i] may contain a string that is in recipes.
 
 # You are also given a string array supplies containing all the ingredients that you initially have, and you have an infinite supply of all of them.
 
@@ -25,11 +27,31 @@
 
 # Example 3:
 
-# Input: recipes = ["bread","sandwich","burger"], ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]], supplies = ["yeast","flour","meat"]
+# Input: recipes = ["bread","sandwich","burger"], 
+# ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]], 
+# supplies = ["yeast","flour","meat"]
 # Output: ["bread","sandwich","burger"]
 # Explanation:
 # We can create "bread" since we have the ingredients "yeast" and "flour".
 # We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
 # We can create "burger" since we have the ingredient "meat" and can create the ingredients "bread" and "sandwich".
-def findAllRecipes(self, recipes: list[int], ingredients: list[list[int]], supplies: list[str]):
-    pass
+import collections
+
+
+def findAllRecipes(recipes: list[str], ingredients: list[list[str]], supplies: list[str]):
+    graph = collections.defaultdict(list)
+    indegree_no = collections.defaultdict(int)
+    for recipe, ingre in zip(recipes, ingredients):
+        indegree_no[recipe] = len(ingre)
+
+        for ingr in ingre:
+            graph[ingr].append(recipe)
+
+
+    print(graph)
+
+recipes = ["bread","sandwich","burger"]
+ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]]
+supplies = ["yeast","flour","meat"]
+# Output: ["bread","sandwich","burger"]
+print(findAllRecipes(recipes, ingredients, supplies))
