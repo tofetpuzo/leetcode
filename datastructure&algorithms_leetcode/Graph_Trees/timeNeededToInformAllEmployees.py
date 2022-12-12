30c6fc9943260b81853c44ddf96402b7d9928b29
tofetpuzo <topetemiola09@gmail.com>
2022-12-12 01:04:59 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/timeNeededToInformAllEmployees.py
+++ datastructure&algorithms_leetcode/Graph_Trees/timeNeededToInformAllEmployees.py
@@ -7,4 +7,41 @@
 # he will inform his direct subordinates, and they will inform their subordinates, and so on
 # the head of the company wants to inform all the company employes of an urgent piece of news.
 # he will inform his direct subordinates, and they will inform their subordinates, so on until all of his direct subordinates, and they will inform their subordinates and so on untill all the employess know about the urgent news. (i.e. after informaTime[i] minutes )
-# all his direct subordinates can start spreading the news 
+# all his direct subordinates can start spreading the news ).
+# return the number of minutes needed to inform all the employess about the urgent news.
+
+
+# Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]
+# Output: 1
+# Explanation: The head of the company with id = 2 is the direct manager of all the employees in the company and needs 1 minute to inform them all.
+# The tree structure of the employees in the company is shown.
+
+import collections
+
+def numofMinutes(n: int, headID: int , manager: list[int], informTime: list[int]):
+    if n <= 1:
+        return 0
+    
+    res = 0
+    graph = collections.defaultdict(list)
+    
+    for idx , parent in enumerate(manager):
+        graph[parent].append(idx)
+        
+
+    queue = collections.deque([(headID, informTime[headID])])
+    while queue:
+        cur_emp , cur_time = queue.popleft()
+        
+        res = max(res, cur_time)
+        for emp in graph[cur_emp]:
+            queue.append((emp, cur_time + informTime[emp]))
+    
+    return res
+        
+n = 6
+headID = 2 
+manager = [2,2,-1,2,2,2]
+informTime = [0,0,1,0,0,0]  
+print(numofMinutes(n=n, headID=headID, manager=manager, informTime=informTime))
+        
