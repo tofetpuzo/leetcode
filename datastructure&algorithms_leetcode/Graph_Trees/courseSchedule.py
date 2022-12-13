ac7717594d165b9b553ddab6a4e39c75a49bda47
tofetpuzo <topetemiola09@gmail.com>
2022-12-13 19:27:34 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/courseSchedule.py
+++ datastructure&algorithms_leetcode/Graph_Trees/courseSchedule.py
@@ -5,8 +5,39 @@
 # given the total number of courses and a list of prerequiste pairs, is it possible for you to finish all course?
 
 # example 1:
-# numCourse = 2, prerequisites = [[1, 0]]
+numCourse = 5
+prerequisites = [[1, 0], [0,2], [1, 3], [1, 4], [3, 4]]
+# numCourse = 4, prerequisites = [[1, 0], [2, 1], [2, 0], [3, 2]]
 # output = true
 
 def courseSchedule(numCourse: int, prerequisites: list[list[int]]):
-    pass
+    
+    preMap= {i : [] for i in range(numCourse) }
+    for course, preps in prerequisites:
+        preMap[course].append(preps)
+    
+    visit = set()
+    def dfs(crs):
+        
+        if crs in visit:
+            return False
+
+        if preMap[crs] == []:
+            return True
+        
+        visit.add(crs)
+        for pre in preMap[crs]:
+            if not dfs(pre): return False
+        visit.remove(crs)
+        preMap[crs] = []
+        return True
+    
+    for crs in range(numCourse):
+        if not dfs(crs): return False
+
+    return True
+  
+
+
+
+print(courseSchedule(numCourse, prerequisites))
