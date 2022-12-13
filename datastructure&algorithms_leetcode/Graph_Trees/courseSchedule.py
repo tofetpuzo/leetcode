bb0cf209f74bd90cbb4893f0ce75b89466a1dcdf
tofetpuzo <topetemiola09@gmail.com>
2022-12-13 17:42:07 +0000

    solution

---
+++ datastructure&algorithms_leetcode/Graph_Trees/courseSchedule.py
@@ -0,0 +1,12 @@
+# There are total of numCourses you have to take, labeled from 0 to numCourses-1
+# some courses may have prerequisties, for example to take course 0 you have to
+# first take course 1 which is expressed as a pair: [0, 1]
+
+# given the total number of courses and a list of prerequiste pairs, is it possible for you to finish all course?
+
+# example 1:
+# numCourse = 2, prerequisites = [[1, 0]]
+# output = true
+
+def courseSchedule(numCourse: int, prerequisites: list[list[int]]):
+    pass
