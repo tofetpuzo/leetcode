2164f888c3b8412de74a0f314c31230326dc5f3a
tofetpuzo <topetemiola09@gmail.com>
2022-12-14 23:09:41 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/mostFreqSumTree.py
+++ datastructure&algorithms_leetcode/Graph_Trees/mostFreqSumTree.py
@@ -9,3 +9,36 @@ class TreeNode:
         self.value = value
         self.left = left
         self.right = right
+
+
+def findFrequentTree(root: TreeNode):
+    if not root:
+        return []
+
+    # freq_dict stores numbers hence why i incremented it.
+
+    freq_dict = collections.defaultdict(int)
+    max_freq = 0
+
+    dfs(root)
+    res = []
+
+    for val, freq in freq_dict.items():
+        if freq == max_freq:
+            res.append(val)
+
+    def dfs(node):
+        if not node:
+            return 0
+
+        if not node.left and not node.right:
+            freq_dict[node.value] += 1
+
+            max_freq = max(max_freq, freq_dict[node.value])
+
+            return node.value
+
+        l_sum = dfs(node.left)
+        r_sum = dfs(node.right)
+
+        cur_sum = node.value + l_sum + r_sum
