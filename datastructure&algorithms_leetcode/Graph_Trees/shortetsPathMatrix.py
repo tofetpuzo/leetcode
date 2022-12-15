c6b2d4847cc687d7e060176fe2f1c476c1611a4d
tofetpuzo <topetemiola09@gmail.com>
2022-12-15 12:19:39 +0000

    solution

---
+++ datastructure&algorithms_leetcode/Graph_Trees/shortetsPathMatrix.py
@@ -0,0 +1,34 @@
+import collections
+
+
+grid = [[0, 0, 0], [1, 1, 0], [0, 0, 0], [0, 1, 1], [0, 0, 0]]
+def shortetsPathMatrix(grid: list[list[int]]):
+
+    if grid[0][0] or grid[-1][-1]:
+        return -1
+
+    direction = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+    ROWS = len(grid)
+    COLS = len(grid[0])
+    grid[0][0] = 1
+
+    queue = collections.deque([(0, 0, 1)])
+
+    while queue:
+        row, col, path_len = queue.popleft()
+
+        if (row, col) == (ROWS-1, COLS -1):
+            return path_len
+
+        for dr , dc in direction:
+            new_r = row+ dr
+            new_c = col + dc
+
+            if new_r in range(ROWS) and new_c in range(COLS) and not grid[new_r][new_c]:
+                grid[new_r][new_c] = 1
+
+                queue.append((new_r, new_c, path_len+1))
+
+    return -1
+
+print(shortetsPathMatrix(grid))
