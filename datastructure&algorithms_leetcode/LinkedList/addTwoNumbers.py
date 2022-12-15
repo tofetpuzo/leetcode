dbb8b3b02dd6011a69b9d29d9c62a23ec022562d
tofetpuzo <topetemiola09@gmail.com>
2022-12-15 13:34:53 +0000

    solution

--- datastructure&algorithms_leetcode/LinkedList/addTwoNumbers.py
+++ datastructure&algorithms_leetcode/LinkedList/addTwoNumbers.py
@@ -16,9 +16,19 @@ class Solution:
         cur = dummy
         
         carry = 0
-        while l1 or l2:
+        while l1 or l2 or carry:
             v1 = l1.value if l1 else 0
             v2 = l2.value if l2 else 0
 
             # calculate the new digits
             val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
