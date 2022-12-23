7fb675d74a34cd18e561a8b96dfe5341984d35cd
tofetpuzo <topetemiola09@gmail.com>
2022-12-23 17:12:00 +0000

    solution

--- datastructure&algorithms_leetcode/LinkedList/reverseNodesInK-Groups.py
+++ datastructure&algorithms_leetcode/LinkedList/reverseNodesInK-Groups.py
@@ -3,6 +3,43 @@
 # then left-out nodes, in the end should remain as it is
 # head = [1, 2, 3, 4, 5] k =2
 # output = [2, 1 , 4, 3, 5]
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-def reverseNodesInKGroups():
-    pass
+def reverseNodesInKGroups(head: ListNode, k: int):
+    dummy = ListNode(0, head)
+    groupPrev = dummy
+
+    while True:
+        kthNode = getKthNode(groupPrev, k)
+        # if kthNode is the last element
+        if not kthNode:
+            break
+
+        
+        # [dummy, 1, 2] ,[3 , 4]
+        #  ^             ^ this is GroupNext
+        groupNext = kthNode.next
+
+        # reverse group
+        prev , curr = kthNode.next, groupPrev.next
+        
+        while curr != groupNext:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        # reversing the list
+        tmp = groupPrev.next
+        groupPrev.next = kthNode
+        groupPrev = tmp
+
+def getKthNode(curr, k):
+    while curr and k > 0:
+        curr = curr.next
+        k -=1
+    return curr
+
