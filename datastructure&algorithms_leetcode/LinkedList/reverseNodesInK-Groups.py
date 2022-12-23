3076302d7a1d59e22079909b0326598e9fc4f703
tofetpuzo <topetemiola09@gmail.com>
2022-12-23 16:23:47 +0000

    solution

---
+++ datastructure&algorithms_leetcode/LinkedList/reverseNodesInK-Groups.py
@@ -0,0 +1,8 @@
+# given a linked list reverse the nodes of a linked list k at a time and return the modified list
+# k is a +ve integer and is less than or equal to the length of the linked list. if the number of node.js is not a multiple of k
+# then left-out nodes, in the end should remain as it is
+# head = [1, 2, 3, 4, 5] k =2
+# output = [2, 1 , 4, 3, 5]
+
+def reverseNodesInKGroups():
+    pass
