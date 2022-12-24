e02c18c8699f25066410d4ec3fd4f70da2b58f3c
tofetpuzo <topetemiola09@gmail.com>
2022-12-24 09:08:17 +0000

    solution

---
+++ datastructure&algorithms_leetcode/LinkedList/partitionList.py
@@ -0,0 +1,12 @@
+# given the head of a linkedlist and a value x, 
+# partition it such that all nodes less than x come before nodes greater than or equal to x.
+# head = [1, 4, 3, 2, 5, 2], x = 3
+# output  = [1, 2, 2, 4, 3, 5]
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def partitionList(head: ListNode, x: int):
+    left , right = ListNode(), ListNode()
