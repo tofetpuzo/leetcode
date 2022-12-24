54b9603cdc7be20eacf584ce00a469e9d0486bf0
tofetpuzo <topetemiola09@gmail.com>
2022-12-24 11:48:01 +0000

    solution

--- datastructure&algorithms_leetcode/LinkedList/cycleLinkedListFlyods.py
+++ datastructure&algorithms_leetcode/LinkedList/cycleLinkedListFlyods.py
@@ -11,16 +11,31 @@
 
 # Return true if there is a cycle in the linked list. Otherwise, return false.
 
-#  
-
 # Example 1:
 
 # Input: head = [3,2,0,-4], pos = 1
 # Output: true
 # Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
-
+  
 # Example 2:
 
 # Input: head = [1,2], pos = 0
 # Output: true
 # Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def hasCycle(self, head: ListNode):
+    slow, fast = head, head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+        
+    return False
+
