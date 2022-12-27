7294b3e629b0cd5bcff0f53b7e51dfc1ebb367d0
tofetpuzo <topetemiola09@gmail.com>
2022-12-27 18:44:42 +0000

    solution

--- Java_Algorithms/src/package/searchArray.java
+++ Java_Algorithms/src/package/searchArray.java
@@ -1,3 +1,16 @@
 public class searchArray {
+  public static void main(String[] args) {
+    int[] num = {1, 2, 3, 4, 5, 9};
+    System.out.println(searchInArray(num, 2));
+  }
+
+public static int searchInArray(int[] intArray, int valueToSearch) {
+      // TODO
+      for (int i = 0; i < intArray.length; i++) {
+        if(intArray[i] == valueToSearch) return i;
+      }
+
+    throw new IllegalArgumentException("No solution");
     
+   }
 }
