95714e5b63f00afd1c928e18b1e3c20bd729a5cd
tofetpuzo <topetemiola09@gmail.com>
2022-12-27 12:48:11 +0000

    solution

--- Java_Algorithms/src/package/twoSum.java
+++ Java_Algorithms/src/package/twoSum.java
@@ -1,3 +1,15 @@
 public class twoSum {
-    
+
+    public static void main(String[] args) {
+        
+    }
+    public static int[] twoSums(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length;
+        
+        
+        return nums;
+
+        
+    }
 }
