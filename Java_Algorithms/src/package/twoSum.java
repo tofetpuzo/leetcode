976551817cbc79bf259d23a87888438f83c0c669
tofetpuzo <topetemiola09@gmail.com>
2022-12-27 13:59:50 +0000

    solution

--- Java_Algorithms/src/package/twoSum.java
+++ Java_Algorithms/src/package/twoSum.java
@@ -1,15 +1,19 @@
 public class twoSum {
 
     public static void main(String[] args) {
-        
+        int[] nums = {1, 2, 3, 4, 5, 9};
+        twoSums(nums, 2);
     }
     public static int[] twoSums(int[] nums, int target) {
+        int[] n = null;
         int left = 0; 
         int right = nums.length;
-        
-        
-        return nums;
-
-        
+        for (int i = 0; i < nums.length; i++) {
+            
+            System.out.println(nums[i]);
+    
+       }
+        return n;
+       
     }
 }
