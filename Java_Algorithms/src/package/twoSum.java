0d313812cedd57e4d8fd08c9a665a3bf3bf35178
tofetpuzo <topetemiola09@gmail.com>
2022-12-27 18:18:26 +0000

    solution

--- Java_Algorithms/src/package/twoSum.java
+++ Java_Algorithms/src/package/twoSum.java
@@ -1,26 +1,22 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class twoSum {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 9};
-        twoSums(nums, 2);
+        int[] num = {1, 2, 3, 4, 5, 9};
+        int[] result  = twoSums(num, 9);
+        System.out.println(Arrays.toString(result));
     }
     public static int[] twoSums(int[] nums, int target) {
-        int[] n = null;
         for (int i = 0; i < nums.length; i++) {
-            Arrays.sort(nums);
-            if(i > 0  && nums[i] == nums[i -1]) {
-                continue;
-            }
-            int left = 0, res = 0, right = nums.length;
-
-
-            
-            System.out.println(nums[i]);
-    
-       }
-        return n;    
+           for (int j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] == target) {
+                return new int[] { i , j};
+             }
+           }
+       }  
+       throw new IllegalArgumentException("No solution");
     }
 }
