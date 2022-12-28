9178bb0cd1852ced1bbbc3054dc11a8676c35089
tofetpuzo <topetemiola09@gmail.com>
2022-12-28 11:46:48 +0000

    solution

--- Java_Algorithms/src/package/LinkedList/SinglyLinkedList.java
+++ Java_Algorithms/src/package/LinkedList/SinglyLinkedList.java
@@ -5,7 +5,7 @@ public class SinglyLinkedList {
     public Node tail;
     public int size;
 
-    public Node createSingLNode(int nodeValue){
+    public Node createSingLNode(int nodeValue) {
         // create new node before assignning them
         head = new Node();
         Node node = new Node();
@@ -15,24 +15,77 @@ public class SinglyLinkedList {
         tail = node;
         size = 1;
         return head;
-              
+
     }
+
     // Insert Node in the sL
     public void insertLinkedList(int nodeValue, int location) {
         Node node = new Node();
         node.value = nodeValue;
-        if (head == null){
+        if (head == null) {
             node = createSingLNode(nodeValue);
             return;
-        } else if(location == 0){
+        } else if (location == 0) {
             node.next = head;
-            head = node;           
-        } else if(location >= size){
+            head = node;
+        } else if (location >= size) {
             node.next = null;
-            
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tNode = tNode.next;
+                index++;
+            }
+            Node nextNode = tNode.next;
+            tNode.next = node;
+            node.next = nextNode;
         }
-        
+        size += 1;
 
+    }
+
+    // Traverse method of SL
+    public void traverseSinglyLinkedList() {
+
+        if (head == null) {
+            System.out.println("this sl does not exist");
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print("->");
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("\n");
+    }
+
+    // search method of SL
+    public boolean searchSinglyLinkedList(int searchNode) {
+
+        if (head == null) {
+            System.out.println("this sl does not exist");
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+
+                if (tempNode.value == searchNode) { 
+                    System.out.print("tthis is the index which it was found:" + "  " + " + "+ i +" in this index");
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("node not found");
+        return false;   
         
     }
-} 
+    // delete a node
+    
+    
+}
--- Java_Algorithms/src/package/Main.java
+++ Java_Algorithms/src/package/Main.java
@@ -3,7 +3,16 @@ import LinkedList.SinglyLinkedList;
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList sl = new SinglyLinkedList();
+        
+        // System.out.println(sl.head.value);
         sl.createSingLNode(5);
-        System.out.println(sl.head.value);
+        sl.insertLinkedList(3, 1);
+        sl.insertLinkedList(45, 2);
+        sl.insertLinkedList(34, 3);
+        sl.insertLinkedList(11, 4);
+        sl.insertLinkedList(345, 5);
+        sl.traverseSinglyLinkedList();
+        sl.searchSinglyLinkedList(11);
+        // System.out.println(sl.head.next.value);
     }
 }
