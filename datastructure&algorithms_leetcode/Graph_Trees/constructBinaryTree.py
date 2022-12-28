9ef5e103f6c0f216328a70c4f397ac9ac08ba84d
tofetpuzo <topetemiola09@gmail.com>
2022-12-28 22:09:27 +0000

    solution

---
+++ datastructure&algorithms_leetcode/Graph_Trees/constructBinaryTree.py
@@ -0,0 +1,28 @@
+# Given preorder and inorder traversal of a tree, construct the binary tree.
+
+# Note:
+# You may assume that duplicates do not exist in the tree.
+
+# For example, given
+
+# preorder = [3,9,20,15,7]
+# inorder = [9,3,15,20,7]
+# Return the following binary tree:
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+
+# output : [3, 9, 20, None, None, 15, 7]
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def buildTree(preorder: list[int], inorder: list[int]):
+    
