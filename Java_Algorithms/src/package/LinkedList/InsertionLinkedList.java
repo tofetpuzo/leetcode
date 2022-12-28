c487d5bf09d757b7eb3d6479c4c8102bf579029e
tofetpuzo <topetemiola09@gmail.com>
2022-12-28 08:55:07 +0000

    solution

---
+++ Java_Algorithms/src/package/LinkedList/InsertionLinkedList.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class InsertionLinkedList {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java_Algorithms/src/package/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class SinglyLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createSingLNode(int nodeValue){
+        // create new node before assignning them
+        head = new Node();
+        
+        return head;
+
+    }
+}
