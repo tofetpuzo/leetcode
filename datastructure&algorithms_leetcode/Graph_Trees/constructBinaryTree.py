89c7d2fdae9d30f6d4ca1617da12303459691d45
tofetpuzo <topetemiola09@gmail.com>
2022-12-28 23:19:27 +0000

    solution

--- datastructure&algorithms_leetcode/Graph_Trees/constructBinaryTree.py
+++ datastructure&algorithms_leetcode/Graph_Trees/constructBinaryTree.py
@@ -25,4 +25,14 @@ class TreeNode:
         self.right = right
         
 def buildTree(preorder: list[int], inorder: list[int]):
+    if not preorder or inorder:
+        return None
+    
+    root = TreeNode(preorder[0])
+    mid = inorder.index(preorder[0])
+    root.left = buildTree(preorder[1:mid+1], inorder[:mid])
+    root.right = buildTree(preorder[mid+1:], inorder[mid+1:])
+    
+    return root
+    
     
