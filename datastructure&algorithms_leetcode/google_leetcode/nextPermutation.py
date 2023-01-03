6db33034d63a1cec52c30f18883cdf31f16165f1
tofetpuzo <topetemiola09@gmail.com>
2023-01-03 13:50:57 +0000

    leect

--- datastructure&algorithms_leetcode/google_leetcode/nextPermutation.py
+++ datastructure&algorithms_leetcode/google_leetcode/nextPermutation.py
@@ -6,13 +6,13 @@
 
 # A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
 
-#     For example, for arr = [1,2,3], the following are all the permutations of 
+#     For example, for arr = [1,2,3], the following are all the permutations of
 #    arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
 
-# The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
-# 
-# More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, 
-# then the next permutation of that array is the permutation that follows it in the sorted container. 
+# The next permutation of an array of integers is the next lexicographically greater permutation of its integer.
+#
+# More formally, if all the permutations of the array are sorted in one container according to their lexicographical order,
+# then the next permutation of that array is the permutation that follows it in the sorted container.
 # If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
 
 #     For example, the next permutation of arr = [1,2,3] is [1,3,2].
@@ -23,7 +23,14 @@
 
 # The replacement must be in place and use only constant extra memory.
 
-#  
+#  [2, 3, 1] [2, 1, 3]
+#         ^
+#  [1, 2, 3] is [1, 3, 2].
+#                   ^
+#  algorithm
+# step 1: let pointer be set to last
+# step 2: check if last number is >= num[i - 1]
+# return exactly number
 
 # Example 1:
 
@@ -40,4 +47,7 @@
 # Input: nums = [1,1,5]
 # Output: [1,5,1]
 
-def swap(nums, id1, id):
+def swap(nums, id1, id2):
+    temp = nums[id1]
+    nums[id1] = nums[id2]
+    nums[id2] = temp
