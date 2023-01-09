6ecce5ea4962fd08215b2f5d6a17585f75669463
tofetpuzo <topetemiola09@gmail.com>
2023-01-09 17:14:28 +0000

    solution

--- datastructure&algorithms_leetcode/google_leetcode/kthLargestElementInArray.py
+++ datastructure&algorithms_leetcode/google_leetcode/kthLargestElementInArray.py
@@ -44,10 +44,10 @@ def findKthLargest(nums: list, k: int):
           
 
 
-# nums = [3,2,3,1,2,4,5,5,6]
-# k = 4
-nums = [3,2,1,5,6,4]
-k = 2
+nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+k = 4
+# nums = [3,2,1,5,6,4]
+# k = 2
 
 
 
